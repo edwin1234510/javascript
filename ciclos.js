b3afe02b23e494090612967762419b3bd2d2b0e2
edwin villabona <edwinvillabona0701@gmail.com>
2024-11-25 11:35:21 -0500

    js

--- ciclos.js
+++ ciclos.js
@@ -9,13 +9,17 @@
 //     console.log("?");
 // } while (false);
 
-let numero = 3;
-let contador = 0;
-for(let i = 1;i <= 30; i++){
-    if (numero % i === 0)
-    contador++;
-    console.log(numero);
+// let numero = 3;
+
+// for(let i = 1;i < 30; i++){
+//     if ((numero % i) === 0)
+//     console.log(numero);
+// }
+
+for (let i = 1; i <= 10; i++){
+    if ( i === 4) {
+        continue
+    }
+    console.log(`item ${i}`);
 }
-
-
 
---
+++ destructuración .js
@@ -0,0 +1,11 @@
+// const numeros = [1,2,3,4];
+// let a = numeros[0];
+// let b = numeros[1];
+// let c = numeros[2];
+// let d = numeros[3];
+// let [a,b,c,d] = numeros;
+// console.log(b);
+
+let persona = {nombre:"edwin", apellido:"villabona"};
+let{nombre:nom,apellido:ape} = persona;
+console.log(nom,ape);
